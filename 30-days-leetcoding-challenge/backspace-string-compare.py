460b9ea9ecff323ec30a4ec2d80a7c556b8e340c
Sunho You <jilldunkelheit@gmail.com>
2020-04-13 16:03:29 -0400

    week 2

---
+++ 30-days-leetcoding-challenge/backspace-string-compare.py
@@ -0,0 +1,37 @@
+# Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+# Example 1:
+
+# Input: S = "ab#c", T = "ad#c"
+# Output: true
+# Explanation: Both S and T become "ac".
+# Example 2:
+
+# Input: S = "ab##", T = "c#d#"
+# Output: true
+# Explanation: Both S and T become "".
+# Example 3:
+
+# Input: S = "a##c", T = "#a#c"
+# Output: true
+# Explanation: Both S and T become "c".
+# Example 4:
+
+# Input: S = "a#c", T = "b"
+# Output: false
+# Explanation: S becomes "c" while T becomes "b".
+
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        i, j = len(S) - 1, len(T) - 1
+        backS = backT = 0
+        while True:
+            while i >= 0 and (backS or S[i] == '#'):
+                backS += 1 if S[i] == '#' else -1
+                i -= 1
+            while j >= 0 and (backT or T[j] == '#'):
+                backT += 1 if T[j] == '#' else -1
+                j -= 1
+            if not (i >= 0 and j >= 0 and S[i] == T[j]):
+                return i == j == -1
+            i, j = i - 1, j - 1
---
+++ 30-days-leetcoding-challenge/last-stone-weight.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        while len(stones) >= 2:
+            stones = sorted(stones, reverse=True)
+            value = stones[0] - stones[1]
+            # print(stones[0], '-', stones[1], '=', value)
+            if value > 0:
+                stones = stones[2:]
+                stones.append(value)
+            else:
+                stones = stones[2:]
+        try:
+            return stones[0]
+        except:
+            return 0
